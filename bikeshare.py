8163babcd2b886eb82971b6e171b230cadf82b57
Alexander Hernández Páez <alexher90@gmail.com>
2020-05-18 15:17:30 -0500

    refactor: changes in the last message

--- bikeshare.py
+++ bikeshare.py
@@ -174,7 +174,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
-            print("Thank you for visiting us!")
+            print("Thank you for visiting us! and ride a bike!")
             break
 
 
